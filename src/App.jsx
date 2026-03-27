49444d0397fda50b9cf104ebf41f8565ef450930
Rabiya <rabiyaaiylchieva@gmail.com>
2026-03-27 13:31:49 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,1 @@
- const App = () => {
-    return (
-        <div>
-            <h1>Bilingual-js 20</h1>
-        </div>
-    )
-}
-export default App
+export const App = () => <h1>Bilingual-js 20</h1>
