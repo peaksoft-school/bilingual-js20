c7857600fcff764f68657de714a85791b34da6e6
osimys2285 <osimys2285@gmail.com>
2026-04-01 14:51:43 +0600

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Footer from './layout/Footer.jsx'
+import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<Footer />)
+createRoot(document.getElementById('root')).render(<App />)
