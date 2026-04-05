bd5ac1a7f8f4191b0bf1bf4cad8691a8a03fdb57
murzabekanapiiaev-debug <murzabekanapiiaev@gmail.com>
2026-04-06 02:49:30 +0600

    added toases

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { ModalApp } from './components/ui/modal-ui/ModalApp'
-
 export const App = () => {
   return (
     <>
       <div>
         <h1>App</h1>
-
-        <ModalApp />
       </div>
-      <div>{/* <Modal isOpen={isOpen} onClose={onclose} /> */}</div>
     </>
   )
 }
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -22,6 +22,7 @@ import PlayCircleBlueIcon from './svgs/play-circle-blue.svg'
 import ViewIcon from './svgs/view.svg'
 import VolumePrimaryIcon from './svgs/volume-primary.svg'
 import VolumeSecondaryIcon from './svgs/volume-secondary.svg'
+import LineErrorToas from './svgs/line-error-toas.svg'
 
 export {
   AverageSoundIcon,
@@ -48,4 +49,5 @@ export {
   ViewIcon,
   VolumePrimaryIcon,
   VolumeSecondaryIcon,
+  LineErrorToas,
 }
