d0a5c63167df7d41c2f80e811f4705197c9a99c3
osimys2285 <osimys2285@gmail.com>
2026-04-13 16:33:50 +0600

    fixed checkbox

--- src/components/ui/Checkbox.jsx
+++ src/components/ui/Checkbox.jsx
@@ -1,44 +1,31 @@
 import { forwardRef } from 'react'
-import { Checkbox as MuiCheckbox } from '@mui/material'
-import { styled } from '@mui/material/styles'
+import { Checkbox as MuiCheckbox, styled, Typography } from '@mui/material'
+import { CheckFillGreenIcon } from '../../assets/icons'
 
-const Icon = styled('span')({
+export const Checkbox = forwardRef(({ onChange, checked, disabled, ...rest }, ref) => (
+  <StyledCheckbox
+    ref={ref}
+    onChange={onChange}
+    checked={checked}
+    disabled={disabled}
+    icon={<StyledIcon variant="span" />}
+    checkedIcon={<img src={CheckFillGreenIcon} alt="checked" width={"25px"}/>}
+    {...rest}
+  />
+))
+
+const StyledCheckbox = styled(MuiCheckbox)({
+  padding: 1.2,
+
+  '&:hover': {
+    backgroundColor: 'transparent',
+  },
+})
+
+const StyledIcon = styled(Typography)({
   width: 22,
   height: 22,
   border: '2px solid #9e9e9e',
   borderRadius: 4,
   backgroundColor: 'transparent',
-})
-
-const CheckedIcon = styled(Icon)({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  '&::after': {
-    content: '"✔"',
-    fontSize: 14,
-    color: '#4caf50',
-  },
-})
-
-export const Checkbox = forwardRef(({ onChange, checked, disabled, ...rest }, ref) => {
-  return (
-    <div>
-      <MuiCheckbox
-        ref={ref}
-        onChange={onChange}
-        checked={checked}
-        disabled={disabled}
-        icon={<Icon />}
-        checkedIcon={<CheckedIcon />}
-        sx={{
-          padding: 1.2,
-          '&:hover': {
-            backgroundColor: 'transparent',
-          },
-        }}
-        {...rest}
-      />
-    </div>
-  )
 })
