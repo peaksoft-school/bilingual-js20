c0eae19dfabbadcd25aa887d42d75bab2eb6c614
murzabekanapiiaev-debug <murzabekanapiiaev@gmail.com>
2026-05-24 17:47:03 +0600

    fix: conflict

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.jsx'
 import { Themes } from './components/ui/Themes.jsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
   <Themes>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Themes>
 )
